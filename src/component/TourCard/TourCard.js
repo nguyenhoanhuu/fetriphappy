7a709c2f700a9bd82b763fa7ae168b872ad2ec22
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2023-05-20 17:01:53 +0700

    fix bug r.map

--- src/component/TourCard/TourCard.js
+++ src/component/TourCard/TourCard.js
@@ -1,34 +1,37 @@
+import React from 'react';
 import classNames from 'classnames/bind';
 import styles from '~/component/TourCard/TourCard.module.scss';
 import TourCardItem from './TourCardItem/TourCardItem';
 import { Carousel } from 'antd';
 import '~/component/TourCard/TourCard.css';
+
 const cx = classNames.bind(styles);
 
-function TouCard({ title = 'Ưu đãi tour giờ chót', data, className, numTour = 3, isSmall, shortenCard }) {
-   const showListTourCard = () => {
-      return data.map((item, index) => {
-         return (
-            <TourCardItem
-               key={index}
-               numberCard={numTour}
-               data={item}
-               isSmall={isSmall}
-               shortenCard={shortenCard}
-            ></TourCardItem>
-         );
-      });
-   };
-   return (
-      <div className={cx('wrapper')}>
-         {title && <h2 className={cx('title', className)}>{title}</h2>}
-         {window.innerWidth <= 908 && isSmall !== true ? (
-            <Carousel>{showListTourCard()}</Carousel>
-         ) : (
-            <div className={cx('list-card-tour')}>{showListTourCard()}</div>
-         )}
-      </div>
-   );
+function TourCard({ title = 'Ưu đãi tour giờ chót', data = [], className, numTour = 3, isSmall, shortenCard }) {
+  const showListTourCard = () => {
+    return data.map((item, index) => {
+      return (
+        <TourCardItem
+          key={index}
+          numberCard={numTour}
+          data={item}
+          isSmall={isSmall}
+          shortenCard={shortenCard}
+        />
+      );
+    });
+  };
+
+  return (
+    <div className={cx('wrapper')}>
+      {title && <h2 className={cx('title', className)}>{title}</h2>}
+      {typeof window !== 'undefined' && window.innerWidth <= 908 && isSmall !== true ? (
+        <Carousel>{showListTourCard()}</Carousel>
+      ) : (
+        <div className={cx('list-card-tour')}>{showListTourCard()}</div>
+      )}
+    </div>
+  );
 }
 
-export default TouCard;
+export default TourCard;
